2a89d712db4286a1ffd21854ff3c2da0e0dc6188
April Nichols <nicholsar@gmail.com>
2018-05-25 23:37:25 -0500

    zip code validated

---
+++ js/events.js
@@ -0,0 +1,22 @@
+const weather = require('./weather');
+
+const checkZip = () => {
+  $(document).keypress((e) => {
+    if (e.key === 'Enter') {
+      const userZip = $('#user-zip').val();
+      if (userZip.length === 5 && $.isNumeric(userZip)) {
+        weather.searchWeather(userZip);
+      } else {
+        alert('Please enter a 5-digit zip code.');
+      }
+    }
+  });
+};
+
+const initializer = () => {
+  checkZip();
+};
+
+module.exports = {
+  initializer,
+};
--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,19 @@ const setKey = (key) => {
   weatherKey = key;
 };
 
+const searchWeather = (zip) => {
+  return new Promise((resolve, reject) => {
+    $.ajax(`http://api.openweathermap.org/data/2.5/weather?q=${zip},us&appid=${weatherKey}&units=imperial`)
+      .done((result) => {
+        resolve(result);
+      })
+      .fail((err) => {
+        reject(err);
+      });
+  });
+};
+
 module.exports = {
   setKey,
-  weatherKey,
+  searchWeather,
 };
